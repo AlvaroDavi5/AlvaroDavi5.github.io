850552f0dea83fbee136e9b0ca29ab751bc86700
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2022-01-27 01:12:16 -0300

    feat: finished header and footer

---
+++ pages/portfolio/scripts/homepage.js
@@ -0,0 +1,28 @@
+
+var enabled = false;
+
+var menuContent = '\
+<nav>\
+	<ul>\
+		<li><a href="#about">About</a></li>\
+		<li><a href="#skills">Skills</a></li>\
+		<li><a href="#projects">Projects</a></li>\
+		<li><a href="#experiences">Experiences</a></li>\
+	</ul>\
+</nav>\
+';
+
+
+function toggleOptionsMenu() {
+	let optionsMenu = document.getElementById("menu-options");
+
+
+	if (!enabled) {
+		optionsMenu.innerHTML = menuContent;
+		enabled = true;
+	}
+	else {
+		optionsMenu.innerHTML = '';
+		enabled = false;
+	}
+}
