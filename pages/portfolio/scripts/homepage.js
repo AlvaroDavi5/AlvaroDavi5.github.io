1fbe1be271aa2c6ad01dba90e48842b2de1681c2
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2022-04-01 02:41:19 -0300

    feat: finished About and Contact sessions

--- pages/portfolio/scripts/homepage.js
+++ pages/portfolio/scripts/homepage.js
@@ -6,8 +6,8 @@ var menuContent = '\
 	<ul>\
 		<li><a href="#about">About</a></li>\
 		<li><a href="#skills">Skills</a></li>\
-		<li><a href="#projects">Projects</a></li>\
 		<li><a href="#experiences">Experiences</a></li>\
+		<li><a href="#projects">Projects</a></li>\
 	</ul>\
 </nav>\
 ';
